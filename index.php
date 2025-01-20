1e6f38984b57d6e476b61653417f721a2889aa39
JustinCredible Studios <justincredible219@gmail.com>
2025-01-19 22:42:16 -0500

    Buttons for each action now created

--- index.php
+++ index.php
@@ -2,3 +2,28 @@
 <!--Author: Justin Miller-->
 <!--Date: January 19, 2024-->
 
+<!DOCTYPE HTML>
+
+<html>
+
+    <head>
+        <title>PHP Mailer</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    </head>
+
+    <body>
+
+        <form id="full" action="full.php">
+            <input type="Submit" value="Full">
+        </form>
+
+        <form id="not-full" action="not-full.php">
+            <input type="Submit" value="Not full">
+        </form>
+
+
+    </body>
+
+
+</html>
